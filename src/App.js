61bb6784c0cb86f047171f89539b6a46f71e5a60
STMO <stmo@mac963000-stmo.lan>
2020-02-12 20:19:03 +0100

    scaffolding create react app, file clean-up, and axios installed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return ( 
+        <div className = "App" >
+            <span style={{fontSize: '1rem'}}> GetHubber </span> <span style={{fontSize: '.7rem'}}>Hitta kodare där kodare är: GitHub </span>
+            
+        </div >
+    );
+}
+
+export default App;
