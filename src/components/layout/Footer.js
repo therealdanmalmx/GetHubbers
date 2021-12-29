e5e7f7bb440ba0de3b9858d60d39a6d0d22f9ca8
therealdanmalmx <danmalmx@gmail.com>
2021-12-29 21:27:02 +0100

    Change select for input fields with flags

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import i18n from '../../i18n'
-
-
+import { CircleFlag } from 'react-circle-flags'
 const changeCountry = (e) => {
 
     const country = e.target.value.split(",")[0]
@@ -13,12 +12,25 @@ const changeCountry = (e) => {
 
 }
 
+
 const Footer = () => {
     const { t } = useTranslation();
 
     return (
         <div id="footer">
-            <select
+            <div className="flags">
+                <h4 style={{color: 'white'}}>{t('chooseCountry')}:</h4>
+                <input type="image" src="https://hatscripts.github.io/circle-flags/flags/gb.svg" title={t('uk')} width="30" style={{marginLeft: '10px'}} onClick={changeCountry} value="United Kingdom, en" />
+                <input type="image" src="https://hatscripts.github.io/circle-flags/flags/pt.svg" title={t('portugal')} width="30" style={{marginLeft: '10px'}} onClick={changeCountry} value="Portugal, pt" />
+                <input type="image" src="https://hatscripts.github.io/circle-flags/flags/se.svg" title={t('sweden')} width="30" style={{marginLeft: '10px'}} onClick={changeCountry} value="Sweden, se" />
+
+            </div>
+            {/* <span class="fib fi-pt"><button onClick={changeCountry} value="Portugal, pt">{t('portugal')}</button></span> 
+            <span class="fib fi-se"><button onClick={changeCountry} value="Sweden, se">{t('sweden')}</button></span>  */}
+            {/* <button onClick={changeCountry} class="fib fi-pt" value="Portugal, pt">{t('portugal')}</button>
+            <button onClick={changeCountry} class="fib fi-se" value="Sweden, se">{t('sweden')}</button> */}
+
+            {/* <select
                 className="choose-country"
                 style={{border: 'none', overflow: 'hidden'}}
                 value={ localStorage.getItem('i18nextLang') }
@@ -26,10 +38,10 @@ const Footer = () => {
                 size="3"
                 onChange={changeCountry}
             >
-                <option style={{marginTop: '10px'}} value="United Kingdom, en">{t('uk')}</option>
+                <option data-imgagesrc={pt} style={{marginTop: '10px'}} value="United Kingdom, en">{t('uk')}</option>
                 <option style={{marginTop: '10px'}} value="Portugal, pt">{t('portugal')}</option>
                 <option style={{marginTop: '10px'}} value="Sweden, se">{t('sweden')}</option>
-            </select>
+            </select> */}
         </div>
     )
 }
