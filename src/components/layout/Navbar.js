d321acd13e99ff1be49faf6d28370d7592e69e76
therealdanmalmx <danmalmx@gmail.com>
2022-01-01 20:01:32 +0100

    Adding styling for codeSearch on mobile

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import { useTranslation} from 'react-i18next'
 
-function Navbar(props) {
+function Navbar() {
     const { t } = useTranslation();
 
     return (
